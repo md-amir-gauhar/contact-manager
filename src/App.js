d34f8b1708fb653dbb0e96f0038cf9fa70917958
md-amir-gauhar <gauharamir15@gmail.com>
2020-09-29 23:12:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Contacts from './components/contacts/Contacts';
 import { Provider } from './context';
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">

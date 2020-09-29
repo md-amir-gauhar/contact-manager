042cac07b3161d5b0bf167ba8eb97cae78c8c30d
md-amir-gauhar <gauharamir15@gmail.com>
2020-09-29 23:17:17 +0530

    Updates

---
+++ precache-manifest.b8de866e0a429b8f432493dd708609ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678786d4e3b08264cfdd603d597fadc6",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "5709827c5b9678967d11",
+    "url": "/contactmanager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ab0e7050d2d3fe091213",
+    "url": "/contactmanager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5709827c5b9678967d11",
+    "url": "/contactmanager/static/js/2.e2de9ace.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.e2de9ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0e7050d2d3fe091213",
+    "url": "/contactmanager/static/js/main.d3f1e1ec.chunk.js"
+  },
+  {
+    "revision": "c0561d3ebf5ed2818d21",
+    "url": "/contactmanager/static/js/runtime-main.b16825a8.js"
+  }
+]);
